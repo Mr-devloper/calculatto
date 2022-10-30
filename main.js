a719bb07d03530ed9636aaf8086bf19b5b7689ad
Mr-devloper <74835904+Mr-devloper@users.noreply.github.com>
2022-10-30 22:16:01 +0600

    bug fixed and improvemant added history elementd

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
  const status = "connected"
 {
+   
+function InputFocus (){
+         document.querySelector('input').focus({focusVisible: true});
+    }
+    InputFocus();
         
         let string = "";
         let buttons =
@@ -7,29 +12,38 @@
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
         if (e.target.innerHTML == '=') {
+            InputFocus();
             
             string = eval(string);
             document.querySelector('input').value = string;
             lastResult = document.querySelector('input').value;
+            string = lastResult;
+            document.querySelector('#data').innerText = lastResult;
+
+            InputFocus();
         }
         else if(e.target.innerHTML == 'C'){
             string = "";
             document.querySelector('input').value = string;
+            InputFocus();
 
         } 
         else if(e.target.innerHTML == 'X'){
             string = string + '*';
             document.querySelector('input').value = string;
+            InputFocus();
 
         }
         else if(e.target.innerHTML == '%'){
             string = string + '/100*';
             document.querySelector('input').value = string;
+            InputFocus();
 
         }
         else if(e.target.innerHTML == 'D'){
             string = string.substring(0, string.length - 1);
             document.querySelector('input').value = string;
+            InputFocus();
 
         }
 
@@ -37,6 +51,7 @@ Array.from(buttons).forEach((button)=>{
             console.log(e.target.innerHTML)
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
+            InputFocus();
             
         }
     })
